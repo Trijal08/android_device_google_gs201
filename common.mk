25aea55e1e0d14d9da4ce485e1ea6e035ec4b017
Ionut Gherman <ghermanionut96@gmail.com>
2024-08-13 11:21:16 +0300

    zuma: add Pixel Camera if exists
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- common.mk
+++ common.mk
@@ -339,6 +339,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-V2-ndk.vendor:64
 endif
 
+# Pixel Camera
+$(call inherit-product-if-exists, vendor/google/camera/camera.mk)
+
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 
