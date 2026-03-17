9ddaa1aa688e1f5bdf49c2fa9ea3a581bc56eb6f
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-03-17 17:59:17 -0400

    Revert "zuma: exclude AudioFX"
    This reverts commit 6e1f501aa1a31032ce9b22437253c5d094952b1a.

--- common.mk
+++ common.mk
@@ -335,9 +335,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     ANGLE
 
-# AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.prebuilt.xml \
